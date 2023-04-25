ee00db8f3e988cda870995e3c51b1f4d537e75aa
Sean Neilan <sean@seanneilan.com>
2023-04-25 14:54:20 -0700

    Adds more tests to linked list test

--- tests/linked_list.test.cpp
+++ tests/linked_list.test.cpp
@@ -5,6 +5,11 @@
 int main() {
     DoublyLinkedList<int> ints;
     ints.push_back(5);
-    ASSERT("asdf", ints.get_front()->data == 5)
-    std::cout << "Im proud of you chelsea\n";
+    ints.push_back(6);
+    ints.push_back(7);
+    ints.push_back(8);
+
+    ASSERT("5", ints.get_front()->data == 5)
+    ints.pop_front();
+    ASSERT("6", ints.get_front()->data == 6)
 };
