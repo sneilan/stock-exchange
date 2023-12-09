9c1262b770d2866caddeca37e0565410b270267c
Sean Neilan <sean@seanneilan.com>
2023-12-09 15:08:57 -0800

    Test case for creating password improvement

--- tests/user_service/user_service.test.cpp
+++ tests/user_service/user_service.test.cpp
@@ -6,12 +6,15 @@
 
 TEST_CASE("LibSodium Test - Creating password") {
   char hashed_password[crypto_pwhash_STRBYTES];
-  // memset hashed_password to all zeros.
+  memset(hashed_password, 0, crypto_pwhash_STRBYTES);
+
+  REQUIRE(hashed_password[0] == 0);
+
   const char *my_password = "my_password";
 
   REQUIRE(crypto_pwhash_str(hashed_password, my_password, strlen(my_password),
                             crypto_pwhash_OPSLIMIT_INTERACTIVE, 
                             crypto_pwhash_MEMLIMIT_INTERACTIVE) != 0);
 
-  spdlog::info("hashed password {}", hashed_password);
+  REQUIRE(hashed_password[0] != 0);
 }
