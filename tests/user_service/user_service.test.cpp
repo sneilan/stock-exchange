5ecc8d75d2b04b77b6fd78646063b392974eafd4
Sean Neilan <sean@seanneilan.com>
2023-12-09 17:05:07 -0800

    Can hash password and authenticate

--- tests/user_service/user_service.test.cpp
+++ tests/user_service/user_service.test.cpp
@@ -4,7 +4,7 @@
 
 #include "../../src/user_service/user_service.h"
 
-TEST_CASE("LibSodium Test - Creating password") {
+TEST_CASE("LibSodium Test - Creating and authenticating password") {
   char hashed_password[crypto_pwhash_STRBYTES];
   memset(hashed_password, 0, crypto_pwhash_STRBYTES);
 
@@ -14,7 +14,14 @@ TEST_CASE("LibSodium Test - Creating password") {
 
   REQUIRE(crypto_pwhash_str(hashed_password, my_password, strlen(my_password),
                             crypto_pwhash_OPSLIMIT_INTERACTIVE, 
-                            crypto_pwhash_MEMLIMIT_INTERACTIVE) != 0);
+                            crypto_pwhash_MEMLIMIT_INTERACTIVE) == 0);
 
   REQUIRE(hashed_password[0] != 0);
+
+  // Make sure hashed password matches hashed version.
+  REQUIRE(crypto_pwhash_str_verify(hashed_password, my_password, strlen(my_password)) == 0);
+
+  // Make sure wrong password does not.
+  const char *wrong_password = "battery staple";
+  REQUIRE(crypto_pwhash_str_verify(hashed_password, wrong_password, strlen(wrong_password)) != 0);
 }
